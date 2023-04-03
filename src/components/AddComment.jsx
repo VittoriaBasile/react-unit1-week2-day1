4b69fa97afbc20c51246e84a59b419e8e8060a72
VittoriaBasile <vichy.basile@gmail.com>
2023-04-03 18:11:09 +0200

    commit

--- src/components/AddComment.jsx
+++ src/components/AddComment.jsx
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import { Col, Container, Form, Row } from "react-bootstrap";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
 
 class AddComment extends Component {
   state = {
@@ -10,6 +10,46 @@ class AddComment extends Component {
     },
   };
 
+  handleChange = (propertyName, propertyValue) => {
+    this.setState({
+      reservation: {
+        ...this.state.reservation,
+        [propertyName]: propertyValue,
+      },
+    });
+  };
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("https://striveschool-api.herokuapp.com/api/comments/", {
+        method: "POST",
+        body: JSON.stringify({ comment: "", rate: "1", elementId: "" }),
+
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDJhYzIyOTY4MzQzMTAwMTRkZWE3NWQiLCJpYXQiOjE2ODA1MjM4MTcsImV4cCI6MTY4MTczMzQxN30.cl_8_IXTwcTmqSKAWw2IIGMg3EQd50hLb9Hwb3xKF0g",
+          "Content-Type": "application/json",
+        },
+      });
+      if (response.ok) {
+        this.setState({
+          comment: {
+            comment: "",
+            rate: "1",
+            elementId: "",
+          },
+        });
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  componentDidMount = () => {
+    this.handleSubmit();
+  };
+
   render() {
     return (
       <>
@@ -17,7 +57,7 @@ class AddComment extends Component {
           <h5 className="fw-bold text-center">Scrivi qui il tuo commento</h5>
           <Row className="justify-content-center">
             <Col>
-              <Form>
+              <Form onSubmit={this.handleSubmit}>
                 <Form.Group className="mb-3" controlId="commento">
                   <Form.Label>commento</Form.Label>
                   <Form.Control
@@ -25,14 +65,16 @@ class AddComment extends Component {
                     rows={3}
                     placeholder="Scrivi il tuo commento"
                     value={this.state.comment.comment}
-                    onChange={(e) => this.setState({ comment: { ...this.state.comment, comment: e.target.value } })}
+                    onChange={(e) => this.handleChange("comment", e.target.value)}
+                    required
                   />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Group className="mb-3" controlId="">
                   <Form.Label>vota il libro</Form.Label>
                   <Form.Select
                     value={this.state.comment.rate}
-                    onChange={(e) => this.setState({ comment: { ...this.state.comment, rate: e.target.value } })}
+                    onChange={(e) => this.handleChange("rate", e.target.value)}
+                    required
                   >
                     <option>1</option>
                     <option>2</option>
@@ -47,9 +89,11 @@ class AddComment extends Component {
                     type="text"
                     placeholder="Scrivi l'element Id"
                     value={this.state.comment.elementId}
-                    onChange={(e) => this.setState({ comment: { ...this.state.comment, elementId: e.target.value } })}
+                    onChange={(e) => this.handleChange("elementId", e.target.value)}
+                    required
                   />
                 </Form.Group>
+                <Button type="submit">INVIA</Button>
               </Form>
             </Col>
           </Row>
